91b4ba7e54e955eb3c4d9de3657deb20a5b97c0a
ishqdehlvi <inzemamhaq05@gmail.com>
2023-11-13 22:44:49 +0530

    pics of sandesh and nikhil added

--- src/lib/data/AppIcons.ts
+++ src/lib/data/AppIcons.ts
@@ -1,10 +1,10 @@
 const AppIcons = [
   { path: '/assets/icons/arman.jpg', name: 'Arman Mansury', key: 3 },
   { path: '/assets/icons/Jaseem.png', name: 'Jaseemuddin', key: 5 },
+  { path: '/assets/icons/sandesh.jpeg', name: 'Sandesh', key: 7 },
   { path: '/assets/icons/inzemam.jpg', name: 'Inzemamul Haq', key: 2 },
   { path: '/assets/icons/manjeet.jpeg', name: 'Manjeet', key: 4 },
   { path: '/assets/icons/Shristi.jpg', name: 'Shristi', key: 1 },
-  { path: '/assets/icons/sandesh.jpeg', name: 'Sandesh', key: 7 },
   { path: '/assets/icons/Shagun.jpg', name: 'Shagun', key: 6 },
   { path: '/assets/icons/affan.webp', name: 'Affan Alam', key: 8 },
   { path: '/assets/icons/Ayush.jpeg', name: 'Ayush Verma', key: 9 },
